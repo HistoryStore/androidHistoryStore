7b1fcad121f57a7139e1f786c0cda4c19ed7d220
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-28 08:50:29 -0500

    add category to product model

--- app/src/main/java/com/icaboalo/historystore/io/ProductApiModel.kt
+++ app/src/main/java/com/icaboalo/historystore/io/ProductApiModel.kt
@@ -7,11 +7,13 @@ import com.google.gson.annotations.SerializedName
  */
 class ProductApiModel {
 
-    constructor(mName: String, mTypeUOM: String, mConversion: String) {
-        this.mName = mName
-        this.mTypeUOM = mTypeUOM
-        this.mConversion = mConversion
+    constructor(name: String, typeUOM: String, conversion: String, categoryId: Int) {
+        this.mName = name
+        this.mTypeUOM = typeUOM
+        this.mConversion = conversion
+        this.mCategoryId = categoryId
     }
+
 
     @SerializedName("id")
     var mId: Int? = null
@@ -27,4 +29,10 @@ class ProductApiModel {
 
     @SerializedName("conversion")
     var mConversion: String
+
+    @SerializedName("category")
+    var mCategory: CategoryApiModel? = null
+
+    @SerializedName("category_id")
+    var mCategoryId: Int
 }
