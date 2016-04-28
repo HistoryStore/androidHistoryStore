3fc1b6b278a196fb9184aec65ab6188ed64b1e95
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-27 20:17:10 -0500

    added purchases fragment

---
+++ app/src/main/java/com/icaboalo/historystore/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.icaboalo.historystore
+
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+
+import butterknife.Bind
+import butterknife.ButterKnife
+
+class MainActivity : AppCompatActivity() {
+
+    @Bind(R.id.navigation_view)
+    internal var mNavigationView: NavigationView? = null
+
+    @Bind(R.id.drawer_layout)
+    internal var mDrawerLayout: DrawerLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val nToolbar = findViewById(R.id.app_bar) as Toolbar?
+        setSupportActionBar(nToolbar)
+        ButterKnife.bind(this)
+        navigationViewOnClick()
+    }
+
+    internal fun navigationViewOnClick() {
+        replaceFragment(Fragment())
+        mNavigationView!!.setNavigationItemSelectedListener { item ->
+            val fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.action_capture_list -> {
+                }
+            }
+            mDrawerLayout!!.closeDrawers()
+            false
+        }
+    }
+
+    internal fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+    }
+}
---
+++ app/src/main/java/com/icaboalo/historystore/PurchasesFragment.kt
@@ -0,0 +1,34 @@
+package com.icaboalo.historystore
+
+import android.os.Bundle
+import android.support.annotation.Nullable
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.Bind
+import butterknife.ButterKnife
+import java.util.*
+
+/**
+ * Created by icaboalo on 27/04/16.
+ */
+class PurchasesFragment: Fragment() {
+
+    @Bind(R.id.purchase_recycler)
+    var mPurchaseRecycler: RecyclerView? = null
+
+    override fun onCreateView(inflater: LayoutInflater, @Nullable container: ViewGroup?, @Nullable savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_purchases, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ButterKnife.bind(this, view)
+    }
+
+    fun setupPurchaseRecycler(purchasesList: ArrayList<String>) {
+
+    }
+}
