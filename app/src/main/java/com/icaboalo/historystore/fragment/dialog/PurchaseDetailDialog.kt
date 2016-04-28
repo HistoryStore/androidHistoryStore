8135176855e611524d067a1ff47b62eb04b08628
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-28 16:05:37 -0500

    added design for dialog

--- app/src/main/java/com/icaboalo/historystore/fragment/dialog/PurchaseDetailDialog.kt
+++ app/src/main/java/com/icaboalo/historystore/fragment/dialog/PurchaseDetailDialog.kt
@@ -1,5 +1,15 @@
 package com.icaboalo.historystore.fragment.dialog
 
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+
 /**
  * Created by icaboalo on 28/04/16.
  */
+class PurchaseDetailDialog: DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
