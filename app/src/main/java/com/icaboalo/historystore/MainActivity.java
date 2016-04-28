643f692706022a0b041b68d6e21f1d9718934eda
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-27 19:56:56 -0500

    main activity java

--- app/src/main/java/com/icaboalo/historystore/MainActivity.java
+++ app/src/main/java/com/icaboalo/historystore/MainActivity.java
@@ -1,13 +1,52 @@
 package com.icaboalo.historystore;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar nToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(nToolbar);
+        ButterKnife.bind(this);
+        navigationViewOnClick();
+    }
+
+    void navigationViewOnClick(){
+        replaceFragment(new Fragment());
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.action_capture_list:
+                        break;
+                }
+                replaceFragment(fragment);
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    void replaceFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
