1536c3f9bbd54cc0001cf8b4ba94748fb3896275
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-29 18:12:09 -0500

    hotfix api models

--- app/src/main/java/com/icaboalo/historystore/io/model/ProductApiModel.kt
+++ app/src/main/java/com/icaboalo/historystore/io/model/ProductApiModel.kt
@@ -8,11 +8,13 @@ import java.io.Serializable
  */
 class ProductApiModel: Serializable {
 
-    constructor(name: String, typeUOM: String, conversion: String, categoryId: Int) {
+    constructor(name: String, typeUOM: String, conversion: String, price:String, categoryId: Int, imageUrl: String) {
         this.mName = name
         this.mTypeUOM = typeUOM
         this.mConversion = conversion
+        this.mPrice = price
         this.mCategoryId = categoryId
+        this.mImageUrl = imageUrl
     }
 
 
@@ -25,11 +27,17 @@ class ProductApiModel: Serializable {
     @SerializedName("name")
     var mName: String
 
+    @SerializedName("image")
+    var mImageUrl: String
+
     @SerializedName("type_uom")
     var mTypeUOM: String
 
     @SerializedName("conversion")
     var mConversion: String
+
+    @SerializedName("price")
+    var mPrice: String
 
     @SerializedName("category")
     var mCategory: CategoryApiModel? = null
