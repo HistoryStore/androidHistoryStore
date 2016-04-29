1d2309a84913cb94ad018c38c5a9a75d2bc5a968
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-29 00:14:38 -0500

    functions on create dialog

--- app/src/main/java/com/icaboalo/historystore/adapter/ProductRecyclerAdapter.kt
+++ app/src/main/java/com/icaboalo/historystore/adapter/ProductRecyclerAdapter.kt
@@ -18,19 +18,17 @@ class ProductRecyclerAdapter: RecyclerView.Adapter<ProductRecyclerAdapter.Produc
     var mContext: Context
     var mProductList: ArrayList<ProductApiModel>
     var mInflater: LayoutInflater
-    var mViewHolderClick: OnViewHolderClick
 
-    constructor(context: Context, purchaseList: ArrayList<ProductApiModel>, viewHolderClick: OnViewHolderClick) : super() {
+    constructor(context: Context, purchaseList: ArrayList<ProductApiModel>) : super() {
         this.mContext = context
         this.mProductList = purchaseList
-        this.mViewHolderClick = viewHolderClick
         mInflater = LayoutInflater.from(context)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProductViewHolder? {
         val view: View = mInflater.inflate(R.layout.item_products, parent, false)
-        return ProductViewHolder(view, R.id.product_text, R.id.quantity_text, R.id.price_text, mViewHolderClick)
+        return ProductViewHolder(view, R.id.product_text, R.id.quantity_text, R.id.price_text)
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
@@ -44,22 +42,17 @@ class ProductRecyclerAdapter: RecyclerView.Adapter<ProductRecyclerAdapter.Produc
         return mProductList.size
     }
 
-    class ProductViewHolder: RecyclerView.ViewHolder, View.OnClickListener {
+    class ProductViewHolder: RecyclerView.ViewHolder{
 
         var mProduct: TextView
         var mQuantity: TextView
         var mPrice: TextView
-        var mViewHolderClick: OnViewHolderClick
 
-        constructor(itemView: View, productTextId: Int, quantityTextId: Int, priceTextId: Int, viewHolderClick: OnViewHolderClick) : super(itemView){
+        constructor(itemView: View, productTextId: Int, quantityTextId: Int, priceTextId: Int): super(itemView){
             mProduct = itemView.findViewById(productTextId) as TextView
             mQuantity = itemView.findViewById(quantityTextId) as TextView
             mPrice = itemView.findViewById(priceTextId) as TextView
-            mViewHolderClick = viewHolderClick
         }
 
-        override fun onClick(v: View) {
-            mViewHolderClick.onClick(v, adapterPosition)
-        }
     }
 }
--- app/src/main/java/com/icaboalo/historystore/fragment/dialog/PurchaseDetailDialog.kt
+++ app/src/main/java/com/icaboalo/historystore/fragment/dialog/PurchaseDetailDialog.kt
@@ -4,9 +4,15 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.icaboalo.historystore.PurchaseApiModel
-import com.icaboalo.historystore.R
+import com.icaboalo.historystore.R.id.*
+import com.icaboalo.historystore.R.layout.dialog_detail_purchase
+import com.icaboalo.historystore.adapter.ProductRecyclerAdapter
 import com.icaboalo.historystore.io.ProductApiModel
 import java.util.*
 
@@ -14,6 +20,11 @@ import java.util.*
  * Created by icaboalo on 28/04/16.
  */
 class PurchaseDetailDialog: DialogFragment() {
+
+    var mProductRecycler: RecyclerView? = null
+    var mVendorText: TextView? = null
+    var mAddressText: TextView? = null
+    var mTotalText: TextView? = null
 
     fun newInstance(purchase: PurchaseApiModel): PurchaseDetailDialog{
         val fragment = PurchaseDetailDialog()
@@ -26,13 +37,34 @@ class PurchaseDetailDialog: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val alertDialog: AlertDialog.Builder = AlertDialog.Builder(activity)
         val inflater = activity.layoutInflater
-        val view = inflater.inflate(R.layout.dialog_detail_purchase, null)
+        val view = inflater.inflate(dialog_detail_purchase, null)
         alertDialog.setView(view)
         alertDialog.setTitle(getPurchase().mPurchaseDate)
+        setupProductRecycler(view, getPurchase().mProducts)
+        setTexts(view)
         return alertDialog.create()
+    }
+
+    fun setTexts(view: View){
+        mVendorText = view.findViewById(vendor_text) as TextView
+        mAddressText = view.findViewById(address_text) as TextView
+        mTotalText = view.findViewById(total_text) as TextView
+
+        mVendorText!!.text = ""
+        mAddressText!!.text = getPurchase().mPlace!!.mAddress
+        mTotalText!!.text = "0"
     }
 
     fun getPurchase(): PurchaseApiModel{
         return arguments.getSerializable("PURCHASE") as PurchaseApiModel
+    }
+
+    fun setupProductRecycler(view: View, productList: ArrayList<ProductApiModel>){
+        mProductRecycler = view.findViewById(product_recycler) as RecyclerView
+        val productRecyclerAdapter: ProductRecyclerAdapter = ProductRecyclerAdapter(activity, productList)
+        val linearLayout: LinearLayoutManager = LinearLayoutManager(activity)
+        mProductRecycler!!.adapter = productRecyclerAdapter
+        mProductRecycler!!.layoutManager = linearLayout
+
     }
 }
